c8a78317ed91c5191d181422b094e4a584cb144c
xhani-manolis-trungu <trmanolis@gmail.com>
2019-09-03 01:08:11 +0300

    Added a small project with Karma and Jasmine Testing on AngularJS

---
+++ app/services/users/users.spec.js
@@ -0,0 +1,102 @@
+describe('Users factory', function () {
+    var Users;
+
+    // The array of users our factory will provide us
+    var userList = [{
+            id: '1',
+            name: 'Jane',
+            role: 'Designer',
+            location: 'New York',
+            twitter: 'gijane'
+        },
+        {
+            id: '2',
+            name: 'Bob',
+            role: 'Developer',
+            location: 'New York',
+            twitter: 'billybob'
+        },
+        {
+            id: '3',
+            name: 'Jim',
+            role: 'Developer',
+            location: 'Chicago',
+            twitter: 'jimbo'
+        },
+        {
+            id: '4',
+            name: 'Bill',
+            role: 'Designer',
+            location: 'LA',
+            twitter: 'dabill'
+        }
+    ];
+
+    var id = '3'
+    //The single user we expect to receive when calling findById('2')
+    var singleUser = {
+        id: `${id}`,
+        name: 'Jim',
+        role: 'Developer',
+        location: 'Chicago',
+        twitter: 'jimbo'
+    };
+    // Load our api.users module
+    beforeEach(angular.mock.module('api.users'));
+
+    // Set our injected Users factory (_Users_) to our local Users variable
+    beforeEach(inject(function (_Users_) {
+        Users = _Users_;
+    }));
+
+    // A simple test to verify the Users service exists
+    it('should exist', function () {
+        expect(Users).toBeDefined();
+    });
+
+    // A set of tests for our Users.all() method
+    describe('.all()', function () {
+        // A simple test to verify the method all exists
+        it('should exist', function () {
+            expect(Users.all).toBeDefined();
+        });
+    });
+
+    // A set of tests for our Users.all() method
+    describe('.all()', function () {
+        // A simple test to verify the method all exists
+        it('should exist', function () {
+            expect(Users.all).toBeDefined();
+        });
+
+        // A test to verify that calling all() returns the array of users we hard-coded above
+        it('should return a hard-coded list of users', function () {
+            expect(Users.all()).toEqual(userList);
+        });
+    });
+
+    // A set of tests for our Users.findById() method
+    describe('.findById()', function () {
+        // A simple test to verify the method findById exists
+        it('should exist', function () {
+            expect(Users.findById).toBeDefined();
+        });
+    });
+
+    describe('.findById(id)', function () {
+        // A simple test to verify the method findById exists
+        it('should exist', function () {
+            expect(Users.findById).toBeDefined();
+        });
+
+        // A test to verify that calling findById() with an id, in this case '2', returns a single user
+        it('should return one user object if it exists', function () {
+            expect(Users.findById(`${id}`)).toEqual(singleUser);
+        });
+
+        // A test to verify that calling findById() with an id that doesn't exist, in this case 'ABC', returns undefined
+        it('should return undefined if the user cannot be found', function () {
+            expect(Users.findById('ABC')).not.toBeDefined();
+        });
+    });
+});
